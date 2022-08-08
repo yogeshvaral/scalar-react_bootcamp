6eab3a7593a8d63db2154624864734a835595f81
Yogesh Varal <yogeshvaralhdp@gmail.com>
2022-08-08 07:38:22 +0530

    cleaing directories

---
+++ src/playground.js
@@ -0,0 +1,13 @@
+var random = "Triple Hi"
+console.log(random)
+
+const first ="First constant"
+// first ="second" //this is not allowed
+
+
+const randomDict = {
+    name:"Yogesh"
+}
+
+randomDict.name = "Kamkinee" //individual properties of const object can be redefined
+console.log(randomDict)
